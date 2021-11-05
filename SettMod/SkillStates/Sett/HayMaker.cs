e04cd99ec6f7f4a2be6072217da84945e5867a96
lemonlust <johnreygun@gmail.com>
2021-11-05 05:22:36 -0800

    cleanup haymaker code

--- SettMod/SkillStates/Sett/HayMaker.cs
+++ SettMod/SkillStates/Sett/HayMaker.cs
@@ -8,43 +8,66 @@ namespace SettMod.SkillStates
 {
     public class HayMaker : BaseSkillState
     {
-        protected float startUp = 0.78f;
-        protected float EarlyExitTime = 1.2f;
-        protected float baseDuration = 1.51f;
-        public static float hayMakerRadius = 55f;
-        public static float hayMakerDamageCoefficient = Modules.Config.hayMakerDamageCoefficient.Value;
-        public static float hayMakerProcCoefficient = 1f;
-        public static float hayMakerGritBonus = Modules.Config.hayMakerGritBonus.Value;
-        public static float hayMakerForce = 1000f;
-        public GameObject blastEffectPrefab = Resources.Load<GameObject>("prefabs/effects/SonicBoomEffect");
-        private float gritSnapShot;
-        private float maxGritSnapShot;
-        private Ray downRay;
-        private Vector3 hitSphereScale = new Vector3(50f, 14f, 14f);
-
         public static Vector3 CameraPosition = new Vector3(1.8f, -2.4f, -8f);
+        public static float hayMakerDamageCoefficient = Modules.Config.hayMakerDamageCoefficient.Value;
+        public static float hayMakerForce = 1000f;
+        public static float hayMakerGritBonus = Modules.Config.hayMakerGritBonus.Value;
+        public static float hayMakerProcCoefficient = 1f;
+        public static float hayMakerRadius = 55f;
+        public GameObject blastEffectPrefab = Resources.Load<GameObject>("prefabs/effects/SonicBoomEffect");
+        public float duration;
+        protected Animator animator;
+        protected float baseDuration = 1.51f;
+        protected float EarlyExitTime = 1.2f;
+        protected float startUp = 0.78f;
+        protected float stopwatch;
+        private float gritSnapShot;
+        private bool hasFired;
+        private float maxGritSnapShot;
         private Transform slamIndicatorInstance;
 
-        private Vector3 punchVector
-
+        public override void FixedUpdate()
         {
-            get
+            base.FixedUpdate();
+            this.stopwatch += Time.fixedDeltaTime;
+
+            if (!this.slamIndicatorInstance) this.CreateIndicator(); this.UpdateSlamIndicator();
+
+            if (this.stopwatch >= this.startUp && !this.hasFired)
             {
-                //return base.characterDirection.forward.normalized;
-                return base.inputBank.aimDirection;
+                this.hasFired = true;
+                Util.PlaySound("SettWVO", base.gameObject);
+                if (NetworkServer.active)
+                {
+                    this.Fire();
+                }
+                for (int i = 0; i <= 20; i++)
+                {
+                    float coneSize = 60f;
+                    Quaternion punchRot = Util.QuaternionSafeLookRotation(this.characterDirection.forward.normalized);
+                    float spreadFactor = 0.01f;
+                    punchRot.x += Random.Range(-spreadFactor, spreadFactor) * coneSize;
+                    punchRot.y += Random.Range(-spreadFactor, spreadFactor) * coneSize;
+                    EffectManager.SpawnEffect(this.blastEffectPrefab, new EffectData
+                    {
+                        origin = this.characterBody.corePosition,
+                        scale = 100f,
+                        rotation = punchRot
+                    }, false);
+                }
+            }
+
+            if (this.stopwatch >= this.EarlyExitTime && base.isAuthority && this.hasFired)
+            {
+                this.outer.SetNextStateToMain();
+                return;
             }
         }
 
-#pragma warning disable CS0169 // The field 'HayMaker.attack' is never used
-        private OverlapAttack attack;
-#pragma warning restore CS0169 // The field 'HayMaker.attack' is never used
-
-        public float duration;
-
-        private bool hasFired;
-        protected Animator animator;
-
-        protected float stopwatch;
+        public override InterruptPriority GetMinimumInterruptPriority()
+        {
+            return InterruptPriority.Skill;
+        }
 
         public override void OnEnter()
         {
@@ -84,15 +107,6 @@ namespace SettMod.SkillStates
         {
         }
 
-        private void UpdateSlamIndicator()
-        {
-            if (this.slamIndicatorInstance)
-            {
-                Vector3 transformLocation = base.transform.position + base.characterDirection.forward * 23f;
-                this.slamIndicatorInstance.transform.position = transformLocation;
-            }
-        }
-
         private void CreateIndicator()
         {
             if (EntityStates.Huntress.ArrowRain.areaIndicatorPrefab)
@@ -101,14 +115,13 @@ namespace SettMod.SkillStates
                 this.slamIndicatorInstance = UnityEngine.Object.Instantiate<GameObject>(EntityStates.Huntress.ArrowRain.areaIndicatorPrefab).transform;
                 this.slamIndicatorInstance.transform.position = transformLocation;
                 this.slamIndicatorInstance.localScale = Vector3.one * 25f;
-
             }
         }
 
         private void Fire()
         {
             Ray aimRay = base.GetAimRay();
-            Collider[] enemies = Physics.OverlapSphere(base.transform.position + base.characterDirection.forward * 23f, this.hitSphereScale.x / 2);
+            Collider[] enemies = Physics.OverlapSphere(base.transform.position + base.characterDirection.forward * 23f, 25f);
             int num = 0;
             int num2 = 0;
             while (num < enemies.Length && num2 < 1000000f)
@@ -143,54 +156,13 @@ namespace SettMod.SkillStates
             }
         }
 
-        public override void FixedUpdate()
+        private void UpdateSlamIndicator()
         {
-            base.FixedUpdate();
-            /*
-            CameraTargetParams ctp = base.cameraTargetParams;
-            float denom = (1 + Time.fixedTime - this.initialTime);
-            float smoothFactor = 8 / Mathf.Pow(denom, 2);
-            Vector3 smoothVector = new Vector3(-3 / 20, 1 / 16, -1);
-            ctp.idealLocalCameraPos = CameraPosition + smoothFactor * smoothVector;
-            */
-            this.stopwatch += Time.fixedDeltaTime;
-
-            if (!this.slamIndicatorInstance) this.CreateIndicator(); this.UpdateSlamIndicator();
-
-            if (this.stopwatch >= this.startUp && !this.hasFired)
+            if (this.slamIndicatorInstance)
             {
-                this.hasFired = true;
-                Util.PlaySound("SettWVO", base.gameObject);
-                if (NetworkServer.active)
-                {
-                    this.Fire();
-                }
-                for (int i = 0; i <= 20; i++)
-                {
-                    float coneSize = 60f;
-                    Quaternion punchRot = Util.QuaternionSafeLookRotation(this.characterDirection.forward.normalized);
-                    float spreadFactor = 0.01f;
-                    punchRot.x += Random.Range(-spreadFactor, spreadFactor) * coneSize;
-                    punchRot.y += Random.Range(-spreadFactor, spreadFactor) * coneSize;
-                    EffectManager.SpawnEffect(this.blastEffectPrefab, new EffectData
-                    {
-                        origin = this.characterBody.corePosition,
-                        scale = 100f,
-                        rotation = punchRot
-                    }, false);
-                }
+                Vector3 transformLocation = base.transform.position + base.characterDirection.forward * 23f;
+                this.slamIndicatorInstance.transform.position = transformLocation;
             }
-
-            if (this.stopwatch >= this.EarlyExitTime && base.isAuthority && this.hasFired)
-            {
-                this.outer.SetNextStateToMain();
-                return;
-            }
-        }
-
-        public override InterruptPriority GetMinimumInterruptPriority()
-        {
-            return InterruptPriority.Skill;
         }
     }
 }
